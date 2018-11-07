2581a8ec2c4d2d682327e45c3d5fa519cd82ba40
ASUS-PC\y.kazuki <ttyoshiyoshi@gmail.com>
2018-11-07 10:56:25 +0900

    Login sample

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 */
+Route::post('login', 'AuthController@login');
+Route::post('logout', 'AuthController@logout');
+Route::post('register', 'AuthController@register');
 
 Route::resource('project', 'ProjectController');
 
